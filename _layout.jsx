298ce65c2661f216f20c69ec87388df4fa46b656
M Hussain Shahzad <hussainshahzad7744@gmail.com>
2026-02-05 11:32:56 +0500

    Gesture drawer with router

--- _layout.jsx
+++ _layout.jsx
@@ -1,11 +1,18 @@
 import { Tabs } from "expo-router";
-
+import { FontAwesome } from "@expo/vector-icons";
+import { Drawer } from "expo-router/drawer";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
 export default function TabLayout() {
   return (
-    <Tabs>
-      <Tabs.Screen name="index" options={{ title: "Home" }} />
-      <Tabs.Screen name="about" options={{ title: "About" }} />
-      <Tabs.Screen name="profile" options={{ title: "Profile" }} />
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <Drawer>
+         <Tabs>
+      <Tabs.Screen name="index" options={{ title: "Home", tabBarIcon: ({ color }) => <FontAwesome size={28} name="home" color={color} /> }} />
+      <Tabs.Screen name="about" options={{ title: "About", tabBarIcon: ({ color }) => <FontAwesome size={28} name="info-circle" color={color} /> }} />
+      <Tabs.Screen name="profile" options={{ title: "Profile", tabBarIcon: ({ color }) => <FontAwesome size={28} name="user" color={color} /> }} />
     </Tabs>
+      </Drawer>
+        
+    </GestureHandlerRootView>
   );
 }
