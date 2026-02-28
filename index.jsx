85613ac0825f517646626ad6ee9e90530cc80d8c
M Hussain Shahzad <hussainshahzad7744@gmail.com>
2026-02-28 12:26:59 +0500

    tyleSheet in react native

--- index.jsx
+++ index.jsx
@@ -1,43 +1,23 @@
-import React from "react";
-import { View, Text, FlatList, StyleSheet } from "react-native";
-
-const data = [
-  { id: "1", name: "Item 1" },
-  { id: "2", name: "Item 2" },
-  { id: "3", name: "Item 3" },
-  { id: "4", name: "Item 4" },
-    { id: "5", name: "Item 4" },
-      { id: "6", name: "Item 4" },
-        { id: "7", name: "Item 4" },
-
-          { id: "8", name: "Item 4" },
-            { id: "9", name: "Item 4" },
-];
-
-export default function App() {
-  return (
-    <FlatList
-      data={data}
-      keyExtractor={(item) => item.id}
-      renderItem={({ item }) => (
-        <View style={styles.box}>
-          <Text style={styles.text}>{item.name}</Text>
-        </View>
-      )}
-    />
-  );
-}
+import { StyleSheet, View, Text } from "react-native";
 
 const styles = StyleSheet.create({
-  box: {
-    height: 80,
-    backgroundColor: "#4caf50",
-    margin: 10,
+  container: {
+    flex: 1,
+    backgroundColor: "#f2f2f2",
     justifyContent: "center",
     alignItems: "center",
   },
   text: {
-    color: "white",
-    fontSize: 18,
+    color: "blue",
+    fontSize: 20,
+    fontWeight: "bold",
   },
 });
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Hello World!</Text>
+    </View>
+  );
+}
