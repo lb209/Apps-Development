8ee8ff651c5c70fab2aa51cc67a94e8f780061ff
M Hussain Shahzad <hussainshahzad7744@gmail.com>
2026-03-10 10:50:59 +0500

    Task 9:(password genrate)

--- index.jsx
+++ index.jsx
@@ -1,12 +1,42 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import React, { useState } from "react";
+import { View, Text, Button, StyleSheet } from "react-native";
 
-export default function index() {
+export default function App() {
+
+  const [randomNumber, setRandomNumber] = useState(0);
+
+  const generateRandom = () => {
+const char="askldjfielkdklsierjkdiaAJLKJADOIJNALDIAJLDNNL;NEORIJAAA12357809892598098290@!#$%^*&!><"
+let pass="";                                  
+for(let i=0;i<10;i++){
+const gan=Math.floor(Math.random()*char.length)
+pass+=char[gan]
+}
+setRandomNumber(pass)
+  };
+
   return (
-    <View>
-      <Text>i am hussain</Text>
+    <View style={styles.container}>
+      
+      <Text style={styles.text}>
+        Random Number: {randomNumber}
+      </Text>
+
+      <Button title="Generate Number" onPress={generateRandom} />
+
     </View>
-  )
+  );
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+
+  text: {
+    fontSize: 24,
+    marginBottom: 20,
+  },
+});
